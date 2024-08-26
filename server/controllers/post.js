fd0e3ddee56b49ee3cfe23b31f72557cfdac4d45
Duy Le <aaron.le1995@gmail.com>
2024-08-26 12:20:08 +0700

    reverse change

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -57,36 +57,27 @@ const getPost = async (req, res) => {
 const editPost = async (req, res) => {
   const { postId } = req.params;
   const { title, content } = req.body;
-  const { email } = req.query;
 
   try {
     const post = await Post.findById(postId);
+    if (!post) return res.status(404).json({ message: "Post not found" });
 
-    if (!post) {
-      return res.status(404).json({ message: "Post not found" });
-    }
-
-    if (email && post.userId.email !== email) {
+    if (post.userId.toString() !== req.user.id)
       return res
         .status(403)
         .json({ message: "You are not authorized to edit this post" });
+
+    if (title || content) {
+      if (title) post.title = title;
+      if (content) post.content = content;
+      const updatedPost = await post.save();
+      return res.status(200).json({
+        message: "Post updated successfully",
+        post: postResponse(updatedPost, req.user.username),
+      });
     }
 
-    if (post.userId.toString() !== req.user.id) {
-      return res
-        .status(403)
-        .json({ message: "You are not authorized to edit this post" });
-    }
-
-    if (title) post.title = title;
-    if (content) post.content = content;
-
-    const updatedPost = await post.save();
-
-    res.status(200).json({
-      message: "Post updated successfully",
-      post: postResponse(updatedPost, req.user.username),
-    });
+    res.status(400).json({ message: "No fields to update" });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
